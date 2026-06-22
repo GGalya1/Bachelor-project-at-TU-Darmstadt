e3329e7ce2bef22c335b810617d119ab72fd78bc
GGalya1 <garturmail@gmail.com>
2026-06-22 20:30:34 +0200

    feat: added event bus for observer pattern

--- Assets/Scripts/Achievements/LevelEvents.cs
+++ Assets/Scripts/Achievements/LevelEvents.cs
@@ -1,16 +1,22 @@
-using UnityEngine;
+using System;
 
-public class LevelEvents : MonoBehaviour
+public static class LevelEvents
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// Fired once when the player submits a correct solution.
+    /// Carries a full snapshot of the result via <see cref="LevelCompletedData"/>.
+    /// </summary>
+    public static event Action<LevelCompletedData> LevelCompleted;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /// <summary>
+    /// Fired each time the player submits a wrong answer.
+    /// Carries the total number of failed tries so far.
+    /// </summary>
+    public static event Action<int> SolutionFailed;
+    
+    public static void RaiseLevelCompleted(LevelCompletedData data)
+        => LevelCompleted?.Invoke(data);
+
+    public static void RaiseSolutionFailed(int totalFailedTries)
+        => SolutionFailed?.Invoke(totalFailedTries);
 }
